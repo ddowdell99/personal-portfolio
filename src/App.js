ab83818c9fa280b72bfdb87c4f158df3d51dbff4
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2023-03-05 21:44:04 -0600

    added skills section with scroller

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar /> 
+      <NavBar />
       <Banner />
-
+      <Skills />
     </div>
   );
 }
