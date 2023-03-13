2757e8317be6606d111149641bd4aa48c31fbb5c
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2023-03-12 22:15:22 -0500

    added links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
+import githubIcon from "../assets/img/github-icon.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 import { HashLink } from "react-router-hash-link";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -77,11 +77,11 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="" />
+              <a href="https://www.linkedin.com/in/diante-dowdell/" target="_blank">
+                <img src={navIcon1} alt="Diante's LinkedIn Page Link" />
               </a>
-              <a href="#">
-                <img src={navIcon2} alt="" />
+              <a href="https://github.com/ddowdell99" target="_blank">
+                <img src={githubIcon} className="filter-green" alt="Diante's GitHub Link" />
               </a>
               <a href="#">
                 <img src={navIcon3} alt="" />
